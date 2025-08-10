87c503f92b10db778038fb9f8e93db3a42e71d52
xSikio <xsikio1@gmail.com>
2025-08-09 22:51:22 -0500

    modificando estilos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
+import FreeCodeCampLogo from './imagenes/freecodecamp-logo.png'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= 'aplicacion-tareas'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img src={FreeCodeCampLogo}
+        className='freecodecamp-logo' />
+        </div>
+        <div className='tareas-lista-principal'>
+          <h1>Mis Tareas</h1>
+
+        </div>
     </div>
   );
+  
 }
 
 export default App;
